f82c9c756b72743f11195617dc2f5f4a2431fe2e
root <root@travis-job-0b9ebbfe-75c8-4b53-b74b-7f98cb0ef5dc>
2020-04-10 20:33:54 -0700

    IP work for background module

---
+++ modules/background.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+function background () {
+  checkrpi
+  checkargn $# 1
+  checkroot
+  url=$1
+  img="${url##*/}"
+  imgloc="/home/pi/$img"
+  ext=${img: -4}
+  if [ $ext == ".png" ] || [ $ext == ".jpg" ] || [ $ext == "jpeg" ] || [ $ext == ".gif" ];
+  then
+    sudo su - pi -c 'wget '"$url"' && env | grep 'DISPLAY' && export DISPLAY=:0.0 && pcmanfm --set-wallpaper '"$img"
+    #echo "background changed to $img"
+    exit 1
+  fi
+  background_help
+}
+
+function background_help {
+  echo
+  echo "  Usage: $BASENAME background <url>"
+  echo
+  echo "  Changes the background of monitor to image from URL"
+  echo "  Make sure the file you are using has extension .png, .jpg, .jpeg, or .gif"
+  echo
+  echo "  Example:"
+  echo "    $BASENAME background https://hewgill.com/~greg/definitive-metal-family-tree.png"
+  echo "    background changed to definitive-metal-family-tree.png"
+  echo
+}
